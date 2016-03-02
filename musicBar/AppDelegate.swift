ff01941ae3574527990e5ae49e2c9a79d5fcc847
Daniel Ma <drailskid@yahoo.com>
2016-03-02 14:05:58 -0800

    Hey kids! Look up lyrics on genius

--- musicBar/AppDelegate.swift
+++ musicBar/AppDelegate.swift
@@ -10,87 +10,101 @@ import Cocoa
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
+  
+  @IBOutlet weak var window: NSWindow!
+  
+  let statusItem = NSStatusBar.systemStatusBar().statusItemWithLength(NSVariableStatusItemLength)
+  let menu = NSMenu()
+  var currentTrack = TrackInfo(album: "", artist: "", track: "", playerState: "")
+  
+  func applicationDidFinishLaunching(aNotification: NSNotification) {
+    let dnc = NSDistributedNotificationCenter.defaultCenter()
+    dnc.addObserver(self, selector: Selector("updateTrackInfo:"), name: "com.apple.iTunes.playerInfo", object: nil)
+    
+    menu.addItem(NSMenuItem(title: "", action: nil, keyEquivalent: ""))
+    menu.addItem(NSMenuItem(title: "Open iTunes", action: Selector("openItunes:"), keyEquivalent: "o"))
+    menu.addItem(NSMenuItem(title: "Lookup lyrics on Genius.com", action: Selector("searchGenius:"), keyEquivalent: "l"))
+    menu.addItem(NSMenuItem.separatorItem())
+    menu.addItem(NSMenuItem(title: "Quit", action: Selector("terminate:"), keyEquivalent: "q"))
+    
+    statusItem.menu = menu
+  }
+  
+  func applicationWillTerminate(aNotification: NSNotification) {
+    let dnc = NSDistributedNotificationCenter.defaultCenter()
+    dnc.removeObserver(self, name: "com.apple.iTunes.playerInfo", object: nil)
+  }
+  
+  func printQuote(sender: AnyObject) {
+    let quoteText = "Never put off until tomorrow what you can do the day after tomorrow."
+    let quoteAuthor = "Mark Twain"
+    
+    print("\(quoteText) — \(quoteAuthor)")
+  }
+  
+  func getFromAnything(anyObject: [NSObject : AnyObject], key: String) -> String {
+    let maybeValue = anyObject[key]
+    
+    if maybeValue == nil { return "" }
+    
+    return maybeValue as! String
+  }
+  
+  func updateTrackInfo(notification: NSNotification) {
+    if let information = notification.userInfo {
+      let album = getFromAnything(information, key: "Album")
+      let track = getFromAnything(information, key: "Name")
+      let artist = getFromAnything(information, key: "Artist")
+      let playerState = getFromAnything(information, key: "Player State")
+      
+      // print("notification: \(information)")
+      
+      let trackInfo = TrackInfo(album: album, artist: artist, track: track, playerState: playerState)
+      currentTrack = trackInfo
+      updateMenuTextWithTrackInfo(trackInfo)
+      updateMenuItemsWithTrackInfo(trackInfo)
+    }
+  }
+  
+  func updateMenuTextWithTrackInfo(info: TrackInfo) {
+    if let button = statusItem.button {
+      let statusIcon = info.playerState == "Playing" ? "▶" : "❚❚"
+      button.title = "\(statusIcon) \(info.track) - \(info.artist)"
+    }
+  }
+  
+  func updateMenuItemsWithTrackInfo(info: TrackInfo) {
+    menu.removeItemAtIndex(0)
+    menu.insertItem(NSMenuItem(title: "Album: \(info.album)", action: nil, keyEquivalent: ""), atIndex: 0)
+  }
+  
+  func openItunes(sender: AnyObject) {
+    NSWorkspace.sharedWorkspace().launchApplication("iTunes.app")
+  }
 
-    @IBOutlet weak var window: NSWindow!
-
-    let statusItem = NSStatusBar.systemStatusBar().statusItemWithLength(NSVariableStatusItemLength)
-    let menu = NSMenu()
-
-    func applicationDidFinishLaunching(aNotification: NSNotification) {
-        let dnc = NSDistributedNotificationCenter.defaultCenter()
-        dnc.addObserver(self, selector: Selector("updateTrackInfo:"), name: "com.apple.iTunes.playerInfo", object: nil)
-        
-        menu.addItem(NSMenuItem(title: "", action: nil, keyEquivalent: ""))
-        menu.addItem(NSMenuItem(title: "Open iTunes", action: Selector("openItunes:"), keyEquivalent: "o"))
-        menu.addItem(NSMenuItem.separatorItem())
-        menu.addItem(NSMenuItem(title: "Quit", action: Selector("terminate:"), keyEquivalent: "q"))
-        
-        statusItem.menu = menu
+  func searchGenius(sender: AnyObject) {
+    let searchQuery = "\(currentTrack.track) \(currentTrack.artist)".stringByAddingPercentEncodingWithAllowedCharacters(NSCharacterSet.URLQueryAllowedCharacterSet())
+    if let searchQuery = searchQuery {
+      let urlString = "https://duckduckgo.com/?q=!ducky+\(searchQuery)+site%3Agenius.com"
+      let url = NSURL(string: urlString)
+      if let unwrappedURL = url {
+        NSWorkspace.sharedWorkspace().openURL(unwrappedURL)
+      }
     }
-
-    func applicationWillTerminate(aNotification: NSNotification) {
-        let dnc = NSDistributedNotificationCenter.defaultCenter()
-        dnc.removeObserver(self, name: "com.apple.iTunes.playerInfo", object: nil)
-    }
-    
-    func printQuote(sender: AnyObject) {
-        let quoteText = "Never put off until tomorrow what you can do the day after tomorrow."
-        let quoteAuthor = "Mark Twain"
-        
-        print("\(quoteText) — \(quoteAuthor)")
-    }
-    
-    func getFromAnything(anyObject: [NSObject : AnyObject], key: String) -> String {
-        let maybeValue = anyObject[key]
-        
-        if maybeValue == nil { return "" }
-        
-        return maybeValue as! String
-    }
-    
-    func updateTrackInfo(notification: NSNotification) {
-        if let information = notification.userInfo {
-            let album = getFromAnything(information, key: "Album")
-            let track = getFromAnything(information, key: "Name")
-            let artist = getFromAnything(information, key: "Artist")
-            let playerState = getFromAnything(information, key: "Player State")
-            
-            // print("notification: \(information)")
-            
-            let trackInfo = TrackInfo(album: album, artist: artist, track: track, playerState: playerState)
-            updateMenuTextWithTrackInfo(trackInfo)
-            updateMenuItemsWithTrackInfo(trackInfo)
-        }
-    }
-    
-    func updateMenuTextWithTrackInfo(info: TrackInfo) {
-        if let button = statusItem.button {
-            let statusIcon = info.playerState == "Playing" ? "▶" : "❚❚"
-            button.title = "\(statusIcon) \(info.track) - \(info.artist)"
-        }
-    }
-    
-    func updateMenuItemsWithTrackInfo(info: TrackInfo) {
-        menu.removeItemAtIndex(0)
-        menu.insertItem(NSMenuItem(title: "Album: \(info.album)", action: nil, keyEquivalent: ""), atIndex: 0)
-    }
-    
-    func openItunes(sender: AnyObject) {
-        NSWorkspace.sharedWorkspace().launchApplication("iTunes.app")
-    }
+  }
 }
 
 class TrackInfo {
-    var album: String
-    var artist: String
-    var track: String
-    var playerState: String
-    
-    init(album: String, artist: String, track: String, playerState: String) {
-        self.album = album
-        self.artist = artist
-        self.track = track
-        self.playerState = playerState
-    }
+  var album: String
+  var artist: String
+  var track: String
+  var playerState: String
+  
+  init(album: String, artist: String, track: String, playerState: String) {
+    self.album = album
+    self.artist = artist
+    self.track = track
+    self.playerState = playerState
+  }
 }
 
